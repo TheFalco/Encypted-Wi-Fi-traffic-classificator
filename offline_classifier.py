56890c07e07c88b3cdcc82d292ad9a72ef473db2
Mattia <mattia.iamundo@mail.polimi.it>
2021-04-28 17:18:50 +0200

    Moved 'load_ml_model' into learner.py

--- offline_classifier.py
+++ offline_classifier.py
@@ -1,69 +1,56 @@
+import pyshark
+
 import utils
-import pickle
+from learner.learner import load_ml_model
 import numpy as np
 
-
-# Load the training data
-try:
-    print("Loading trained models...")
-    up_lrnd_model, down_lrnd_model = pickle.load(open("learner/trained_model.sav", "rb"))
-    print("Done")
-except (OSError, IOError) as e:
-    # If not present, start the learning
-    print("Trained models not found.\nInitializing learner.py...")
-    from learner.learner import down_model
-    # up_lrnd_model = up_model
-    down_lrnd_model = down_model
-
-# Open input variables
-data = utils.open_json("input_data.json", "config")
-
 # Global variables
-STA = data["STA"]
-AP = data["AP"]
-FILE = data["offline_file"]
-DELIM = data["percentile"]
+DELIM = 300
 
-# Open file to classify
-cap = utils.open_pcapng(FILE, "offline_captures")
 
-# Variables
-up_frame_size = []
-avg_up_frame_size = 0
-avg_up_data_rate = 0
-down_frame_size = []
-avg_down_frame_size = 0
-avg_down_data_rate = 0
+def classify(path: str, sta: str, ap: str):
+    # Variables
+    up_frame_size = []
+    down_frame_size = []
 
-print("Starting offline analysis...")
+    # try to load the model, otherwise create a new one
+    down_lrnd_model = load_ml_model()
 
-# Extrapolate the data from the file
-for pck in cap:
-    # We need to manage malformed packets
-    try:
-        if pck.highest_layer == 'DATA':
-            # Upstream
-            if pck.wlan.da == AP and pck.wlan.sa == STA:
-                up_frame_size.append(int(pck.frame_info.len))
-            # Downstream
-            elif pck.wlan.da == STA and pck.wlan.sa == AP:
-                down_frame_size.append(int(pck.frame_info.len))
-    except:
-        # print("", end="")
-        pass
-cap.close()
+    # Open file to classify
+    cap = pyshark.FileCapture(path)
 
-# Calculate the mean of the extrapolated data
-# to_predict_up = [np.mean(up_frame_size), np.std(up_frame_size),
-#                  (sum(i < DELIM for i in up_frame_size) / len(up_frame_size))]
-to_predict_down = [np.mean(down_frame_size), np.std(down_frame_size),
-                   (sum(i < DELIM for i in down_frame_size) / len(down_frame_size))]
+    print("Starting offline analysis...")
 
-# Run the prediction
-# result_up = up_lrnd_model.predict([to_predict_up])
-result_down = down_lrnd_model.predict([to_predict_down])
+    # Extrapolate the data from the file
+    for pck in cap:
+        # We need to manage malformed packets
+        try:
+            if pck.highest_layer == 'DATA':
+                # Upstream
+                if pck.wlan.da == ap and pck.wlan.sa == sta:
+                    up_frame_size.append(int(pck.frame_info.len))
+                # Downstream
+                elif pck.wlan.da == sta and pck.wlan.sa == ap:
+                    down_frame_size.append(int(pck.frame_info.len))
+        except:
+            # print("", end="")
+            pass
+    cap.close()
 
-# TODO: Classification based on results
-print(result_down)
-print("Analisys complited: ", end=" ")
-print(result_down[0])
+    # Calculate the mean of the extrapolated data
+    # to_predict_up = [np.mean(up_frame_size), np.std(up_frame_size),
+    #                  (sum(i < DELIM for i in up_frame_size) / len(up_frame_size))]
+    to_predict_down = [np.mean(down_frame_size), np.std(down_frame_size),
+                       (sum(i < DELIM for i in down_frame_size) / len(down_frame_size))]
+
+    predict(down_lrnd_model, to_predict_down)
+
+
+def predict(model, capture):
+    # Run the prediction
+    # result_up = up_lrnd_model.predict([to_predict_up])
+    result_down = model.predict([capture])
+
+    print(result_down)
+    print("Analisys complited: ", end=" ")
+    print(result_down[0])
