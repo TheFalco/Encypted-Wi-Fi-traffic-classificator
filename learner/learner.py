358bd096207249b4b206d734cc6ae592080aa010
Mattia <mattia.iamundo@mail.polimi.it>
2021-05-09 14:51:59 +0200

    Fixed, save path for the trained model

--- learner/learner.py
+++ learner/learner.py
@@ -77,7 +77,7 @@ def save_model(model):
     :param model: the trained model
     """
     # Save the model to disk
-    filename = 'learner/trained_model.sav'
+    filename = './trained_model.sav'
     pickle.dump(model, open(filename, 'wb'))
     print("Models correctly saved")
 
@@ -89,7 +89,7 @@ def load_ml_model():
     """
     try:
         print("Loading trained model...")
-        learned_model = pickle.load(open("learner/trained_model.sav", "rb"))
+        learned_model = pickle.load(open("./trained_model.sav", "rb"))
         print("Done")
         return learned_model
     except (OSError, IOError):
